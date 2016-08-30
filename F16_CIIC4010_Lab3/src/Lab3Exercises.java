cb6b930f057ac3eeca5d9a839c88c5441ed3b313
Christian Santiago <christian.santiago23@upr.edu>
2016-08-30 19:33:54 -0400

    Changed JFram name, location pop up and size.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Christian Santiago");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(600, 250);
+		myFrame.setLocation(600, 200);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
